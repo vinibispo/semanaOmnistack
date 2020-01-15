5c26dd0ef81e6fafbbbbdaab5d3e0d8571eb8eea
vinibispo <vini.bispo015@gmail.com>
2020-01-15 00:33:56 -0300

    PointSchema

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -1,3 +1,20 @@
 const {Router} = require('express')
+const axios = require('axios')
 const route = Router()
+const Dev = require('./models/Dev')
+const stringToArray = require('./utils/index')
+route.post('/devs', async (request, response) =>{
+    const {github_username, techs} = request.body
+    const apiResponse = await axios.get(`https://api.github.com/users/${github_username}`)
+    const {name = login, avatar_url, bio} = apiResponse.data
+    const techArray = stringToArray(techs)
+    const dev = await Dev.create({
+        github_username,
+        name,
+        avatar_url,
+        bio,
+        techs: techArray
+    })
+    return response.json(dev)
+})
 module.exports = route

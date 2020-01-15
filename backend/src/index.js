e84be87178064fb0679bb651ef8fb11871b98921
vinibispo <vini.bispo015@gmail.com>
2020-01-15 09:25:18 -0300

    adding cors and so much things styling main

--- backend/src/index.js
+++ backend/src/index.js
@@ -4,9 +4,11 @@ if(process.env.NODE_ENV !== "production") {
 const mongoose = require('mongoose')
 const express = require('express')
 const routes = require('./routes')
+const cors = require('cors')
 const app = express()
 app.use(express.json())
 app.use(routes)
+app.use(cors())
 mongoose.connect(process.env.MYURL, {useNewUrlParser: true, useUnifiedTopology: true})
 
 app.listen(3333)

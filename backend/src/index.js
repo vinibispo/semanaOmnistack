7d9344e6dc9d72c4d8901861269b866eb60b340e
vinibispo <vini.bispo015@gmail.com>
2020-01-14 23:21:36 -0300

    adding new file

--- backend/src/index.js
+++ backend/src/index.js
@@ -3,13 +3,13 @@ if(process.env.NODE_ENV !== "production") {
     )}
 const mongoose = require('mongoose')
 const express = require('express')
+const routes = require('./routes')
 const app = express()
+app.use(routes)
 app.use(express.json())
 app.use(express.urlencoded({
     extended: false
 }))
 mongoose.connect(process.env.MYURL, {useNewUrlParser: true, useUnifiedTopology: true})
-
-
 
 app.listen(3333)
---
+++ backend/src/routes.js
@@ -0,0 +1,3 @@
+const {Router} = require('express')
+const route = Router()
+module.exports = route

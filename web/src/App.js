dbe3a431605d609dda68bff772e0abed6201b3f2
vinibispo <vini.bispo015@gmail.com>
2020-01-15 08:15:10 -0300

    renaming jsx to js again

--- web/src/App.js
+++ web/src/App.js
@@ -4,7 +4,32 @@ import './App.css'
 function App() {
   return (
     <div id="app">
-      <aside></aside>
+      
+      <aside>
+      <strong>Cadastrar</strong>
+
+        <form>
+          <div className="input-block">
+          <label htmlFor="github_username">Usuário do Github</label>
+          <input type="text" name="github_username" id="github_username" required/>
+          </div>
+          <div className="input-block">
+          <label htmlFor="techs">Tecnologias</label>
+          <input type="text" name="techs" id="techs" required/>
+          </div>
+          <div className="input-group">
+          <div className="input-block">
+          <label htmlFor="latitude">Latitude</label>
+          <input type="text" name="latitude" id="latitude" required/>
+          </div>
+          <div className="input-block">
+          <label htmlFor="github_username">Longitude</label>
+          <input type="text" name="longitude" id="longitude" required/>
+          </div>
+          </div>
+          <button type="submit">Salvar</button>
+        </form>
+      </aside>
       <main></main>
     </div>
   )

3d2b19a0315634fe1a3e2813cb1eb8420a345ca1
vinibispo <vini.bispo015@gmail.com>
2020-01-15 00:46:47 -0300

    abstracting

---
+++ backend/src/controllers/DevController.js
@@ -0,0 +1,30 @@
+const axios = require('axios')
+const Dev = require('../models/Dev')
+const stringToArray = require('../utils/index')
+
+
+module.exports = {
+   async store(request, response) {
+        const {github_username, techs, longitude, latitude} = request.body
+        let dev = await Dev.findOne({github_username})
+        if(!dev){
+            const apiResponse = await axios.get(`https://api.github.com/users/${github_username}`)
+            const {name = login, avatar_url, bio} = apiResponse.data
+            const techArray = stringToArray(techs)
+            const location = {
+                type: 'Point',
+                coordinates: [longitude, latitude]
+            }
+           dev = await Dev.create({
+                github_username,
+                name,
+                avatar_url,
+                bio,
+                techs: techArray,
+                location
+            })
+    
+        }
+                return response.json(dev)
+    }
+}
--- backend/src/routes.js
+++ backend/src/routes.js
@@ -1,25 +1,5 @@
 const {Router} = require('express')
-const axios = require('axios')
 const route = Router()
-const Dev = require('./models/Dev')
-const stringToArray = require('./utils/index')
-route.post('/devs', async (request, response) =>{
-    const {github_username, techs, longitude, latitude} = request.body
-    const apiResponse = await axios.get(`https://api.github.com/users/${github_username}`)
-    const {name = login, avatar_url, bio} = apiResponse.data
-    const techArray = stringToArray(techs)
-    const location = {
-        type: 'Point',
-        coordinates: [longitude, latitude]
-    }
-    const dev = await Dev.create({
-        github_username,
-        name,
-        avatar_url,
-        bio,
-        techs: techArray,
-        location
-    })
-    return response.json(dev)
-})
+const DevController = require('./controllers/DevController')
+route.post('/devs', DevController.store)
 module.exports = route

fc53d4dc22502bbadc1c35417360193e4762e298
vinibispo <vini.bispo015@gmail.com>
2020-01-15 00:39:39 -0300

    it's working

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -4,16 +4,21 @@ const route = Router()
 const Dev = require('./models/Dev')
 const stringToArray = require('./utils/index')
 route.post('/devs', async (request, response) =>{
-    const {github_username, techs} = request.body
+    const {github_username, techs, longitude, latitude} = request.body
     const apiResponse = await axios.get(`https://api.github.com/users/${github_username}`)
     const {name = login, avatar_url, bio} = apiResponse.data
     const techArray = stringToArray(techs)
+    const location = {
+        type: 'Point',
+        coordinates: [longitude, latitude]
+    }
     const dev = await Dev.create({
         github_username,
         name,
         avatar_url,
         bio,
-        techs: techArray
+        techs: techArray,
+        location
     })
     return response.json(dev)
 })

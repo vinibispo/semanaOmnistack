5adb6afbeb64e8af10c0c7492dae83cf54a0bd4a
vinibispo <vini.bispo015@gmail.com>
2020-01-16 08:09:19 -0300

    let's work with react-native-maps

--- mobile/src/routes.js
+++ mobile/src/routes.js
@@ -5,8 +5,25 @@ import Profile from './pages/Profile'
 
 const Routes = createAppContainer(
     createStackNavigator({
-        Main,
-        Profile
+        Main:{
+            screen: Main,
+            navigationOptions:{
+                title: 'DevRadar'
+            }
+        },
+        Profile:{
+            screen: Profile,
+            navigationOptions:{
+                title: 'Perfil no Github'
+            }
+        }
+    },{
+        defaultNavigationOptions: {
+            headerTintColor: '#fff',
+            headerStyle:{
+                backgroundColor: '#7d40e7'
+            }
+        }
     })
 )
 export default Routes

d80e368c8469293da4614cda284563074a40673f
vinibispo <vini.bispo015@gmail.com>
2020-01-19 11:02:58 -0300

    setting up socketio in event connect

--- backend/src/websocket.js
+++ backend/src/websocket.js
@@ -1,8 +1,18 @@
 const socketio = require('socket.io')
+const connections = []
+const parseStringAsArray = require('./utils/index')
 exports.setupWebSocket = (server) => {
     const io = socketio(server)
     io.on('connection', socket => {
         console.log(socket.id)
-        console.log(socket.handshake.query)
+        const {latitude, longitude, techs} = socket.handshake.query
+        connections.push({
+            id: socket.id,
+            coordinates:{
+                latitude: Number(latitude),
+                longitude: Number(longitude)
+            },
+            techs: parseStringAsArray(techs)
+        })
     })
 }

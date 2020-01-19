f3e74642acb1728101c032d2360579cadcf2c44b
vinibispo <vini.bispo015@gmail.com>
2020-01-19 10:38:29 -0300

    setup for socket in mobile

--- mobile/src/services/socket.js
+++ mobile/src/services/socket.js
@@ -1,3 +1,16 @@
 import socketio from 'socket.io-client'
 
-const socket = socketio('')
+const socket = socketio('http://192.168.0.107:3333', {
+    autoConnect: false
+})
+const connect = ()  => socket.connect()
+
+const disconnect = () =>{
+    if(socket.connected){
+        socket.disconnect()
+    }
+}
+export{
+    connect,
+    disconnect
+}

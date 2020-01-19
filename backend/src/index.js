75bdd7d3449caa971c66b8282d141d811fe04038
vinibispo <vini.bispo015@gmail.com>
2020-01-19 09:54:07 -0300

    adding socketio in backend

--- backend/src/index.js
+++ backend/src/index.js
@@ -2,13 +2,17 @@ if(process.env.NODE_ENV !== "production") {
     require('dotenv').config({path: '.env'}
     )}
 const mongoose = require('mongoose')
+const http = require('http')
 const express = require('express')
 const routes = require('./routes')
+const {setupWebSocket} = require('./websocket')
 const cors = require('cors')
 const app = express()
+const server = http.Server(app)
+setupWebSocket(server)
 app.use(cors())
 app.use(express.json())
 app.use(routes)
 mongoose.connect(process.env.MYURL, {useNewUrlParser: true, useUnifiedTopology: true})
 
-app.listen(3333)
+server.listen(3333)
---
+++ backend/src/websocket.js
@@ -0,0 +1,7 @@
+const socketio = require('socket.io')
+exports.setupWebSocket = (server) => {
+    const io = socketio(server)
+    io.on('connection', socket => {
+        console.log(socket.id)
+    })
+}

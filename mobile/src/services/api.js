2ed529aa001624fd4d31a4b3fb0c09d725494ced
vinibispo <vini.bispo015@gmail.com>
2020-01-19 10:35:47 -0300

    adding new url

--- mobile/src/services/api.js
+++ mobile/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://172.16.42.209:3333',
+  baseURL: 'http://192.168.0.107:3333',
 });
 
 export default api;
--- mobile/src/services/socket.js
+++ mobile/src/services/socket.js
@@ -1,1 +1,3 @@
-import socket from ''
+import socketio from 'socket.io-client'
+
+const socket = socketio('')

e58579f1b1198e1dc7a6ca6f224f3cd31fa98d2b
vinibispo <vini.bispo015@gmail.com>
2020-01-19 10:55:05 -0300

    let's work harder with socketio

--- backend/src/websocket.js
+++ backend/src/websocket.js
@@ -3,5 +3,6 @@ exports.setupWebSocket = (server) => {
     const io = socketio(server)
     io.on('connection', socket => {
         console.log(socket.id)
+        console.log(socket.handshake.query)
     })
 }
